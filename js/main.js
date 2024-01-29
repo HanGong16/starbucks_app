e95125f657f79a1e6549611817db245ec51d1a3b
gong16 <hgim96715@gmail.com>
2024-01-29 21:24:58 +0900

    feat : Add fade-in js
    - forEach 함수 활용
    - gsap.to활용 (gsap Library)
    -index+1인 이유는 0부터 시작하기때문

--- js/main.js
+++ js/main.js
@@ -39,3 +39,11 @@ window.addEventListener(
     }
   }, 300)
 );
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7,
+    opacity: 1,
+  });
+});
