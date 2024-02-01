a204e3b1e6ee0f1805d80d88b376583ba608a1c9
gong16 <hgim96715@gmail.com>
2024-02-01 20:16:30 +0900

    feat: Add floating Animation

--- js/main.js
+++ js/main.js
@@ -92,3 +92,19 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionIcon.innerHTML = 'expand_circle_up';
   }
 });
+function random(min, max) {
+  return parseFloat(Math.random() * (max - min) + min);
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1,
+    yoyo: true,
+    ease: 'power1.inOut',
+    delay: random(0, delay),
+  });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', 0.5, 15);
+floatingObject('.floating3', 1.5, 20);
